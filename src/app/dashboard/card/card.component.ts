9774bd3a55fcb6f5f1e862f2528b5351ddde7258
Erllon <erllonemanuel11@gmail.com>
2023-07-31 23:19:12 -0300

    Modificação do componente card

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { LikesService } from "./../../services/likes.service";
+import { Component, OnInit, Input } from "@angular/core";
+import { MatSnackBar } from "@angular/material";
+import { ICard } from "src/app/interfaces/card.interface";
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"],
 })
 export class CardComponent implements OnInit {
+  @Input() card: ICard;
+  buttonColor: string;
+  constructor(private likesService: LikesService, private _snackBar: MatSnackBar) { }
 
-  @Input() card;
-  constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  onLike() {
+    this.card.likes++;
+    if (this.card.likes >= 5) {
+      this.buttonColor = "blue";
+    }
+    if (this.card.likes > 10) {
+      this.buttonColor = "orange";
+      this._snackBar.open(`Mais de ${this.card.likes - 1} likes nesse card!`, "Fechar", { duration: 2000 });
+    }
   }
 
-  onLike(card: any){
-    // TODO: incrementar o like, salvar via rest
-  }
-
-  onShare(card: any){
+  onShare() {
     // TODO: abrir o link do seu linkedin
+    window.open("https://www.linkedin.com/in/erllon-nowacki/", "_blank");
+    this._snackBar.open("Compartilhando...", "Fechar", { duration: 5000 });
   }
-
 }
